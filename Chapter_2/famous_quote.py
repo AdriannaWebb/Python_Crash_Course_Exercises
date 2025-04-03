071b35383887756881d3799375bd0fc7199bd1cf
Adrianna Webb <anna.m.webb110@gmail.com>
2025-04-03 13:32:02 -0500

    added chapter 2 exercises

---
+++ Chapter_2/famous_quote.py
@@ -0,0 +1,13 @@
+'''
+2-5. Famous Quote: Find a quote from a famous person you 
+admire. Print the quote and the name of its author. 
+Your output should look something like the following, 
+including the quotation marks:
+
+Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+'''
+
+author_name = 'unknown'
+quote = 'You say it would take forever so you will not start, but the time will pass anyway '
+
+print(f"it is {author_name} who once said, '{quote}'")
